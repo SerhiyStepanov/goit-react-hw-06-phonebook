6c77077c256b78b05c1c3aa1f334bef3615b17a0
SerhiyStepanov <SerhiyStepanov@users.noreply.github.com>
2021-01-19 18:23:27 +0200

    add folder redux

---
+++ src/redux/reducer.js
@@ -0,0 +1,40 @@
+import { createReducer } from "@reduxjs/toolkit";
+import { combineReducers } from "redux";
+import actions from "./actions";
+// import types from "./types";
+
+const items = createReducer([], {
+  [actions.addContact]: (state, { payload }) => [...state, payload],
+  [actions.deleteContact]: (state, { payload }) =>
+    state.filter((contact) => contact.id !== payload),
+});
+// const items = (state = [], { type, payload }) => {
+//   switch (type) {
+//     case types.ADD:
+//       return [...state, payload];
+
+//     case types.DELETE:
+//       return state.filter((contact) => contact.id !== payload);
+
+//     default:
+//       return state;
+//   }
+// };
+const filter = createReducer("", {
+  [actions.filterContacts]: (state, { payload }) => payload,
+});
+
+// const filter = (state = "", { type, payload }) => {
+//   switch (type) {
+//     case types.FILTER:
+//       return payload;
+
+//     default:
+//       return state;
+//   }
+// };
+
+export default combineReducers({
+  items,
+  filter,
+});
